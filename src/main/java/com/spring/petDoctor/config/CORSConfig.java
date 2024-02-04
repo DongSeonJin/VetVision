3ddfac77d845ecf2906c7bba7f713b145d4dd8b9
Dongja <129161266+jds79793@users.noreply.github.com>
2024-02-05 07:00:44 +0900

    전역 CORS 설정

---
+++ src/main/java/com/spring/petDoctor/config/CORSConfig.java
@@ -0,0 +1,19 @@
+package com.spring.petDoctor.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("http://34.64.184.19:8080",
+                        "http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
