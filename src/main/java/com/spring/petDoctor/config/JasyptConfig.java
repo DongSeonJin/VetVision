cd4d5712114a668daaf57a1f77ed99ad4e396ffd
Dongja <129161266+jds79793@users.noreply.github.com>
2024-02-05 01:22:19 +0900

    jasypt config 파일 수정

--- src/main/java/com/spring/petDoctor/config/JasyptConfig.java
+++ src/main/java/com/spring/petDoctor/config/JasyptConfig.java
@@ -23,7 +23,6 @@ public class JasyptConfig {
         config.setPoolSize("1");
         config.setProviderName("SunJCE");
         config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
         config.setStringOutputType("base64");
         encryptor.setConfig(config);
         return encryptor;
